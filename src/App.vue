a255fe54f83f173ea6e6f05f260ea2282cd4901a
gaganpandher52 <gaganpandher52@gmail.com>
2020-08-04 17:24:45 -0500

    worked on the main page component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/GitHub-Mark.png">
+    <HelloWorld msg="Git-Plus"/>
   </div>
 </template>
 
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img{
+  height: 200px;
+  width: 200px;
 }
 </style>
